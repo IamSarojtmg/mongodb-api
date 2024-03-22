afa0059e6ecfc0aea236d7ba10a67ddf4d779b09
Saroj <YOUR_EMAIL>
2024-03-22 15:40:32 +0000

    put method defined

--- controller/getProduct.controller.js
+++ controller/getProduct.controller.js
@@ -1,26 +1,44 @@
-const Product = require('../models/products.model')
+const Product = require("../models/products.model");
 
 const getProduct = async (req, res) => {
-try {
-    const products = await Product.find()
-   res.status(200).json({products})
-} catch (error) {
-    res.status(500).json({message: 'internal server error'})
-}
-}
+  try {
+    const products = await Product.find();
+    res.status(200).json({ products });
+  } catch (error) {
+    res.status(500).json({ message: "internal server error" });
+  }
+};
 
 const getProductByID = async (req, res) => {
-    try {
-        const { Id } = req.params
+  try {
+    const { Id } = req.params;
 
-        const product = await Product.findById(Id)
+    const product = await Product.findById(Id);
 
-        res.status(200).json({product})
+    res.status(200).json({ product });
+  } catch (error) {
+    res.status(500).json({ message: "internal server error" });
+  }
+};
 
-} catch (error) {
-    res.status(500).json({message: 'internal server error'})
-}
-}
+const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
 
+    const product = await Product.findByIdAndUpdate(id, req.body); // does not work with out this
 
-module.exports = {getProduct, getProductByID}
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `Product with the id of ${id} not found` });
+    }
+
+    const updatedProduct = await Product.findById(id);
+
+    res(200).json({ updatedProduct });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getProduct, getProductByID, updateProduct };
