50efab4abb2fa69963624a3ca4f4575913290674
Saroj <YOUR_EMAIL>
2024-03-24 12:01:57 +0000

    formatted

--- tests/create-products.test.js
+++ tests/create-products.test.js
@@ -55,45 +55,45 @@ describe("Get products by ID", () => {
 
     const response = await request(app).post("/products").send(newProduct);
 
-    const respond = await request(app).get(`/products/660013fed2e7420ec6f695f7`);
+    const respond = await request(app).get(
+      `/products/660013fed2e7420ec6f695f7`
+    );
     expect(respond.status).toBe(200);
-    expect(respond.body.product._id).toBe('660013fed2e7420ec6f695f7');
+    expect(respond.body.product._id).toBe("660013fed2e7420ec6f695f7");
     expect(respond.body.product.name).toBe("xbox360");
   });
 });
 
 describe("Update the prodcut by ID", () => {
-  it('update the name from ps6 to ps5', async () => {
-
-    let id = '660013fdd2e7420ec6f695f2'
+  it("update the name from ps6 to ps5", async () => {
+    let id = "660013fdd2e7420ec6f695f2";
 
     const updatedName = {
-      name: 'Sony Ps5'
-    }
+      name: "Sony Ps5",
+    };
 
-    const response = await request(app).put(`/products/${id}`).send(updatedName)
+    const response = await request(app)
+      .put(`/products/${id}`)
+      .send(updatedName);
 
-    const respond = await request(app).get(`/products/${id}`)
+    const respond = await request(app).get(`/products/${id}`);
 
-    expect(respond.body.product.name).toBe('Sony Ps5')
-
-  })
-})
+    expect(respond.body.product.name).toBe("Sony Ps5");
+  });
+});
 
 describe("Delete the product by id", () => {
-  it('404 - deletes the specific product by id and returns no body', async() => {
-    let id = '660009b976773921dece005e'
+  it("404 - deletes the specific product by id and returns no body", async () => {
+    let id = "660009b976773921dece005e";
 
-    const response = await request(app).delete(`/products/${id}`)
+    const response = await request(app).delete(`/products/${id}`);
     if (response.status === 200) {
-      expect(response.body.message).toBe(`product with ${id} deleted`)
+      expect(response.body.message).toBe(`product with ${id} deleted`);
     } else {
-      expect(response.body.message).toBe(`${id} not found`)
+      expect(response.body.message).toBe(`${id} not found`);
     }
-    
-
-  })
-})
+  });
+});
 
 afterAll(async () => {
   await mongoose.connection.close();
